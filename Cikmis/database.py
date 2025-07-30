8d2f908897f9ffd49818e29560c02ecb99d63231
Mehmet Halit Barut <mhbarut66@gmail.com>
2025-07-30 22:18:16 +0300

    fix: Improve database connection pool settings for production

--- Cikmis/database.py
+++ Cikmis/database.py
@@ -6,7 +6,11 @@ import config
 
 DATABASE_URL = config.settings.database_url
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_recycle=300,  # 300 saniye (5 dakika) boşta kalan bağlantıları otomatik olarak yenile
+    pool_pre_ping=True # Havuzdan bir bağlantı almadan önce "canlı mı?" diye test et
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
